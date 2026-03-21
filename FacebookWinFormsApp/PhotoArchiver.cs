66d489c9b9587a41c05e3fd0501611c8bbf2ab25
YossiYad <yadgar360@gmail.com>
2026-03-21 19:38:52 +0200

    השלמת photoArchiver

--- FacebookWinFormsApp/PhotoArchiver.cs
+++ FacebookWinFormsApp/PhotoArchiver.cs
@@ -1,12 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using FacebookWrapper.ObjectModel;
 
 namespace BasicFacebookFeatures
 {
-    class PhotoArchiver
+    public class PhotoArchiver
     {
+        private readonly User r_LoggedInUser;
+
+        public event Action<int, int> ProgressChanged;
+
+        public PhotoArchiver(User i_LoggedInUser)
+        {
+            r_LoggedInUser = i_LoggedInUser;
+        }
+
+        public List<Photo> GetPhotosOlderThan(int i_YearsLimit)
+        {
+            List<Photo> oldPhotos = new List<Photo>();
+            DateTime limitDate = DateTime.Now.AddYears(-i_YearsLimit);
+
+            foreach (Album album in r_LoggedInUser.Albums)
+            {
+                foreach (Photo photo in album.Photos)
+                {
+                    if (photo.CreatedTime < limitDate)
+                    {
+                        oldPhotos.Add(photo);
+                    }
+                }
+            }
+            return oldPhotos;
+        }
+
+        public void DownloadPhotos(List<Photo> i_Photos, string i_DestinationPath)
+        {
+            int currentPhotoIndex = 0;
+            int totalPhotos = i_Photos.Count;
+
+            using (WebClient client = new WebClient())
+            {
+                foreach (Photo photo in i_Photos)
+                {
+                    string photoUrl = photo.PictureURL;
+                    string fileName = string.Format("{0}_{1}.jpg", photo.Id, photo.CreatedTime.ToString("yyyyMMdd_HHmmss"));
+                    string fullPath = Path.Combine(i_DestinationPath, fileName);
+                    
+                    try
+                    {
+                        client.DownloadFile(photoUrl, fullPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error downloading photo {0}: {1}", photo.Id, ex.Message);
+                    }
+                    
+                    currentPhotoIndex++;
+                    OnProgressChanged(currentPhotoIndex, totalPhotos);
+                }
+            }
+        }
+
+        private void OnProgressChanged(int i_Current, int i_Total)
+        { 
+                ProgressChanged?.Invoke(i_Current, i_Total);
+        }
     }
 }
