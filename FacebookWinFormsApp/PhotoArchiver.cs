7c90701e173597dd6814eff580a19268fd33fc0d
YossiYadgar <yadgar360@gmail.com>
2026-03-24 05:57:37 +0200

    סיום - רק לצריך לברר למה friends analyzer לא עובד

--- FacebookWinFormsApp/PhotoArchiver.cs
+++ FacebookWinFormsApp/PhotoArchiver.cs
@@ -6,68 +6,69 @@ using FacebookWrapper.ObjectModel;
 
 namespace BasicFacebookFeatures
 {
-    public class PhotoArchiver
-    {
-        private readonly User r_LoggedInUser;
-        public event Action<int, int> ProgressChanged;
+	public class PhotoArchiver
+	{
+		private readonly User r_LoggedInUser;
+		public event Action<int, int> ProgressChanged;
 
-        public PhotoArchiver(User i_LoggedInUser)
-        {
-            r_LoggedInUser = i_LoggedInUser;
-        }
+		public PhotoArchiver(User i_LoggedInUser)
+		{
+			r_LoggedInUser = i_LoggedInUser;
+		}
 
-        public List<Photo> GetPhotosOlderThan(int i_YearsLimit)
-        {
-            List<Photo> oldPhotos = new List<Photo>();
-            DateTime limitDate = DateTime.Now.AddYears(-i_YearsLimit);
+		public List<Photo> GetPhotosOlderThan(int i_YearsLimit)
+		{
+			List<Photo> oldPhotos = new List<Photo>();
+			DateTime limitDate = DateTime.Now.AddYears(-i_YearsLimit);
 
-            foreach (Album album in r_LoggedInUser.Albums)
-            {
-                foreach (Photo photo in album.Photos)
-                {
-                    if (photo.CreatedTime != null && photo.CreatedTime < limitDate) // FIXED
-                    {
-                        oldPhotos.Add(photo);
-                    }
-                }
-            }
-            return oldPhotos;
-        }
+			foreach (Album album in r_LoggedInUser.Albums)
+			{
+				foreach (Photo photo in album.Photos)
+				{
+					if (photo.CreatedTime != null && photo.CreatedTime < limitDate)
+					{
+						oldPhotos.Add(photo);
+					}
+				}
+			}
 
-        public void DownloadPhotos(List<Photo> i_Photos, string i_DestinationPath)
-        {
-            int currentPhotoIndex = 0;
-            int totalPhotos = i_Photos.Count;
+			return oldPhotos;
+		}
 
-            using (WebClient client = new WebClient())
-            {
-                foreach (Photo photo in i_Photos)
-                {
-                    if (photo.CreatedTime != null && photo.PictureNormalURL != null) //ADDED 
-                    {
-                        string photoUrl = photo.PictureNormalURL;
-                        string fileName = string.Format("{0}_{1}.jpg", photo.Id, photo.CreatedTime.Value.ToString("yyyyMMdd_HHmmss"));
-                        string fullPath = Path.Combine(i_DestinationPath, fileName);
+		public void DownloadPhotos(List<Photo> i_Photos, string i_DestinationPath)
+		{
+			int currentPhotoIndex = 0;
+			int totalPhotos = i_Photos.Count;
 
-                        try
-                        {
-                            client.DownloadFile(photoUrl, fullPath);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine("Error downloading photo {0}: {1}", photo.Id, ex.Message);
-                        }
+			using (WebClient client = new WebClient())
+			{
+				foreach (Photo photo in i_Photos)
+				{
+					if (photo.CreatedTime != null && photo.PictureNormalURL != null)
+					{
+						string photoUrl = photo.PictureNormalURL;
+						string fileName = string.Format("{0}_{1}.jpg", photo.Id, photo.CreatedTime.Value.ToString("yyyyMMdd_HHmmss"));
+						string fullPath = Path.Combine(i_DestinationPath, fileName);
 
-                        currentPhotoIndex++;
-                        OnProgressChanged(currentPhotoIndex, totalPhotos);
-                    }
-                }
-            }
-        }
+						try
+						{
+							client.DownloadFile(photoUrl, fullPath);
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine("Error downloading photo {0}: {1}", photo.Id, ex.Message);
+						}
 
-        protected virtual void OnProgressChanged(int i_Current, int i_Total)
-        { 
-                ProgressChanged?.Invoke(i_Current, i_Total);
-        }
-    }
+						currentPhotoIndex++;
+						OnProgressChanged(currentPhotoIndex, totalPhotos);
+					}
+				}
+			}
+		}
+
+		protected virtual void OnProgressChanged(int i_Current, int i_Total)
+		{
+			ProgressChanged?.Invoke(i_Current, i_Total);
+		}
+	}
 }
